6d8973aa7b3a74735f40b191d0b018c01edb4d06
janethuangg <jh6248@nyu.edu>
2020-04-25 12:19:13 -0400

    added config file for gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: gunicorn -c gunicorn.conf.py app:app
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,1 +1,5 @@
-limit-request-line = 0
+import multiprocessing
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+limit_request_line = 0
